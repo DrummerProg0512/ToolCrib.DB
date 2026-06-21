641e8f1457a7b7a8f0c917bf7676884595061a3f
Cesar Gallardo <kaesar_gallardo@hotmail.com>
2026-06-21 11:26:13 -0600

    Added Tables: * AreaLocationRelations * UsersAreas

---
+++ ToolCrib.DB/dbo/Tables/AreaLocationRelations.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [dbo].[AreaLocationRelations] (
+    [AreaLocationRelationID] INT           IDENTITY (1, 1) NOT NULL,
+    [AreaLocationParentID]   INT           NOT NULL,
+    [AreaLocationChildID]    INT           NOT NULL,
+    [UpdatedBy]              INT           NOT NULL,
+    [UpdatedOn]              DATETIME2 (7) CONSTRAINT [DF_AreaLocationRelations_UpdatedOn] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_AreaLocationRelations] PRIMARY KEY CLUSTERED ([AreaLocationRelationID] ASC),
+    CONSTRAINT [FK_AreaLocationRelations_AreaLocations] FOREIGN KEY ([AreaLocationParentID]) REFERENCES [dbo].[AreaLocations] ([AreaLocationID]),
+    CONSTRAINT [FK_AreaLocationRelations_AreaLocations1] FOREIGN KEY ([AreaLocationChildID]) REFERENCES [dbo].[AreaLocations] ([AreaLocationID])
+);
+
+
+GO
+
---
+++ ToolCrib.DB/dbo/Tables/UsersAreas.sql
@@ -0,0 +1,20 @@
+CREATE TABLE [dbo].[UsersAreas] (
+    [UsersAreasID]    INT           IDENTITY (1, 1) NOT NULL,
+    [AreaLocationID]  INT           NOT NULL,
+    [UserID]          INT           NOT NULL,
+    [UpdatedOn]       DATETIME2 (7) NOT NULL,
+    [UpdatedBy]       INT           NOT NULL,
+    [UsersAreaActive] BIT           CONSTRAINT [DF_UsersAreas_UsersAreaActive] DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_UsersAreas] PRIMARY KEY CLUSTERED ([UsersAreasID] ASC),
+    CONSTRAINT [FK_UsersAreas_AreaLocations] FOREIGN KEY ([AreaLocationID]) REFERENCES [dbo].[AreaLocations] ([AreaLocationID]),
+    CONSTRAINT [FK_UsersAreas_Users] FOREIGN KEY ([UserID]) REFERENCES [dbo].[Users] ([UserID])
+);
+
+
+GO
+
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'getdate()', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'UsersAreas', @level2type = N'COLUMN', @level2name = N'UpdatedOn';
+
+
+GO
+
